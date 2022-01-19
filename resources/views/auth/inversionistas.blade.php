a5a5cb744b0ab23c94776c30cdb877bb841c64bc
HuamanAngel <oxipusioxx12@gmail.com>
2022-01-19 00:59:36 -0500

    COlocado interfaces

--- resources/views/auth/inversionistas.blade.php
+++ resources/views/auth/inversionistas.blade.php
@@ -6,7 +6,7 @@
 @endsection
 
 @section('contenido_cSS')
-
+    {{-- <link rel="stylesheet" href="{{ asset('css/taiwind/style.css') }} "> --}}
 @endsection
 
 
@@ -21,19 +21,25 @@
             }            
         </style>
 
-        <div class="d-flex align-items-start row">
-            <div class="nav flex-column nav-pills col-md-2 col-sm-12" id="v-pills-tab" role="tablist"
+        <div class="flex gap-x-4 d-flex ">
+            <div class="w-1/4 m-8 nav flex-column nav-pills col-md-2  col-sm-12" id="v-pills-tab" role="tablist"
                 aria-orientation="vertical">
                 {{-- <button class="nav-link" id="v-pills-home-tab" data-bs-toggle="pill" data-bs-target="#v-pills-home"
                     type="button" role="tab" aria-controls="v-pills-home" aria-selected="true">Perfil</button> --}}
                 <button class="nav-link active"  id="v-pills-profile-tab" data-bs-toggle="pill"
                     data-bs-target="#v-pills-profile" type="button" role="tab" aria-controls="v-pills-profile"
-                    aria-selected="false">Mis propuestas</button>
+                    aria-selected="false"> 
+                    <img src=" {{ asset('img/icons/icon-props.svg') }}" class="inline mr-8" />
+                    &nbsp; Mis propuestas
+                </button>
                 <button class="nav-link"  id="v-pills-messages-tab" data-bs-toggle="pill"
                     data-bs-target="#v-pills-messages" type="button" role="tab" aria-controls="v-pills-messages"
-                    aria-selected="false">Inversionistas</button>
+                    aria-selected="false">
+                    <img src="{{ asset('img/icons/icon-invers.svg') }}" class="inline mr-8" />
+
+                    Inversionistas</button>
             </div>
-            <div class="tab-content col-md-10 col-sm-10" id="v-pills-tabContent">
+            <div class="w-3/4 tab-content col-md-10 col-sm-10" id="v-pills-tabContent">
                 <div class="tab-pane fade" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">
                 </div>
                 <div class="tab-pane fade show active" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">
@@ -48,7 +54,7 @@
                         <div class="col-sm-3">
                             <div class="card">
                                 <img class="cursor-pointer" data-bs-toggle="modal" data-bs-target="#exampleModal"
-                                    height="200px" src="{{ asset('img/icon_add.png') }}" alt="">
+                                    height="200px" src="{{ asset('img/plus-add.png') }}" alt="">
                             </div>
                         </div>
                         @if (auth()->user()->userPropuestas != null)
@@ -67,7 +73,7 @@
                 </div>
                 <div class="tab-pane fade " id="v-pills-messages" role="tabpanel"
                     aria-labelledby="v-pills-messages-tab">
-                    <caption class="fw-bold text-center py-2">Interesados en tus propuestas</caption>
+                    <h2 class="text-center"> Interesados en tus propuestas</h2>
                     <div class="table-responsive">
                         <table class="table caption-top">
 
@@ -149,9 +155,9 @@
                         </div>
                     </div>
                     <div class="modal-footer">
-                        <input type="submit" name="comprar" value="Comprar" class="btn btn-warning">
-                        <input type="submit" name="gratis" value="Subir gratis" class="btn btn-primary">
-                        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cerrar</button>
+                        <input type="submit" name="comprar" value="Comprar" style="background-color: #FE9900;color:white" class="btn btn-warning">
+                        <input type="submit" name="gratis" value="Subir gratis" style="background-color: #0091D1;color:white" class="btn btn-primary">
+                        <button type="button" class="btn btn-danger" style="color:white" data-bs-dismiss="modal">Cerrar</button>
                     </div>
                 </div>
             </form>
